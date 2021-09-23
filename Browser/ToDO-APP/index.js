b7f46de2ac04c6f5a0f20abbb94eba15aef721bf
nrcool <nomanalam7214@gmail.com>
2021-09-23 10:45:55 +0200

    todo app

---
+++ Browser/ToDO-APP/index.js
@@ -0,0 +1,21 @@
+
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+let ul = document.querySelector("ul")
+
+function addtask(){
+
+        let li = document.createElement("li")
+        let span= document.createElement("span")
+        span.innerText= "X" //<span>X</span>
+        li.innerText= input.value //<li> task 2 </li>
+        li.append(span)//<li> task 2  <span>X</span> </li>
+        
+        //parentNode.insertBefore( newElement, targetElement)
+
+        ul.insertBefore(li, ul.firstElementChild)
+        input.value=""
+}
+
+button.addEventListener("click",addtask )
+
