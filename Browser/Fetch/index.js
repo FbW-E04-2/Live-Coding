3649691641f99d64c53e5fd0d401a856b287cd53
nrcool <nomanalam7214@gmail.com>
2021-09-29 13:03:21 +0200

    fetch

---
+++ Browser/Fetch/index.js
@@ -0,0 +1,57 @@
+//API 
+//Application programming interface
+/* fetch("https://reqres.in/api/users?page=2") //asynchronous code
+.then(response=>response.json())
+.then(result=>console.log(result))
+
+fetch("https://jsonplaceholder.typicode.com/users")//asynchronous code
+.then(response=>response.json() )
+.then(result=>console.log(result)) */
+
+console.log("start")
+
+setTimeout(()=>{
+    console.log("i am timeout")
+},0)
+
+
+console.log("end")
+
+/* let MyPromise= new Promise((resolve,reject)=>{
+    resolve("hello world")
+}) //synchronous code
+
+console.log(MyPromise)//synchronous code
+
+
+MyPromise
+.then(result=>console.log(result)) *///asynchronous code
+
+
+
+
+//synchronous code
+/* console.log(window) //synchronous code
+
+console.log("in start") */ //synchronous code
+
+/* fetch(url).then( cb) .then(cb) */
+
+
+
+
+
+
+/* 
+console.log("in end") */ //synchronous code
+
+
+
+/* let student=["item","item2"]
+
+let data = JSON.stringify(student) //convert into string //synchronous cod
+console.log(typeof data)
+
+
+let objectagain= JSON.parse(data) // parse data back to original form //synchronous cod
+console.log(objectagain) */
