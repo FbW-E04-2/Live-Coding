3abd972d790b3e1f4193a710be6f087fa25594df
nrcool <nomanalam7214@gmail.com>
2021-09-30 10:43:24 +0200

    formdata

--- Browser/Form-Data/index.js
+++ Browser/Form-Data/index.js
@@ -1,10 +1,9 @@
-let form = document.querySelector("form")
+let form = document.querySelector("form");
 
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   //first way to get data from all input field
- /*    let inputs = document.querySelectorAll("input") //NodeList
+  /*    let inputs = document.querySelectorAll("input") //NodeList
 let obj ={}
     inputs.forEach(inp=>{
         if(inp.type==="checkbox" || inp.type==="radio"){
@@ -20,7 +19,7 @@ let obj ={}
     obj[textarea.name]=textarea.value
   
     console.log(obj) */
-    //second way to get data from form
+  //second way to get data from form
 
   /* let username = form.elements["user"].value;
     let email = form.elements["email"].value;
@@ -32,23 +31,43 @@ let obj ={}
         password: form.elements["password"].value
     } */
 
-    //third way to get data from form
+  //third way to get data from form
 
-    let formData = new FormData(form)
-    console.dir(formData)
+  let formData = new FormData(form);
+ 
 
-    let obj={}
-    for(let pair of formData){ // ["user", "value"]
-        obj[pair[0]]  = pair[1]
-    } 
+  console.log([...formData])
+let obj = {}
+let skills=[]
+  for(let item of formData){
+      if(item[0]==="skill"){
+        skills.push(item[1])
+      }else{
+          obj[item[0]] = item[1]
+      }
+  }
 
-    console.log(obj)
+  obj.skill=skills
 
-/* 
+  console.log(obj)
+
+
+  /* 
     console.log(formData.get("user"))
     console.log(formData.get("html"))
     console.log(formData.get("css"))
     console.log(formData.get("gender")) 
     console.log(formData.get("message")) */
+});
 
-}) 
+/* let array = [
+  ["item1", "item2"],
+  ["item3", "item4"],
+  ["item5", "item6"],
+  ["item7", "item8"],
+  ["item9", "item10"],
+];
+
+for (let item of array) {
+  console.log(item[1]);
+} */
