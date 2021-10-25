1d7fd497ed139dfc502d6feef51a87862cbefc4d
nrcool <nomanalam7214@gmail.com>
2021-10-25 11:03:10 +0200

    dom event load unload

---
+++ Browser/load-unload-event/index.js
@@ -0,0 +1,14 @@
+/* window.addEventListener("load",()=>{
+    console.log("page loaded")
+})
+ */
+
+/* window.addEventListener("DOMContentLoaded",()=>{
+    console.log("content loaded")
+}) */
+
+
+/* window.addEventListener("beforeunload",(e)=>{
+    e.preventDefault()
+    alert("hello")
+}) */
