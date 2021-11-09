da5d35045d729050fb39dd3178bf73ac611b79b9
nrcool <nomanalam7214@gmail.com>
2021-11-09 16:02:33 +0100

    add styling to cart

--- React-live-Coding/React-E-Commerce/src/components/Cart.js
+++ React-live-Coding/React-E-Commerce/src/components/Cart.js
@@ -5,15 +5,15 @@ export default function Cart() {
     const {cart} = useContext(MyContext)
     console.log(cart)
     return (
-        <div>
-            <h1>Your Cart Items</h1>
+        <div style={{margin:"0 auto", width:"80%",textAlign:"center",background:"linear-gradient(30deg,black,yellow)",color:"white",boxShadow:"5px 5px 20px black"}}>
+            <h1 style={{backgroundColor:"black"}}>Your Cart Items</h1>
             {cart.map(item=>{
                 return(
-                    <div>
-                        <div className="image">
-                            <img src={item.image}  width="100" height="100 "alt="" />
+                    <div style={{display:"flex",justifyContent:"center",borderBottom:"2px dotted gray",padding:"10px"}}>
+                        <div className="image" >
+                            <img style={{borderRadius:"50%",boxShadow:"2px 2px 10px gray"}} src={item.image}  width="100" height="100 "alt="" />
                         </div>
-                        <div className="desc">
+                        <div className="desc" style={{width:"60%",padding:"10px"}}>
                             <h3>{item.title}</h3>
                             <p>{item.quantity}</p>
                         </div>
