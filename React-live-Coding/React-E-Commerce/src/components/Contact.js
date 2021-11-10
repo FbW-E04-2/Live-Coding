e25271f9f01084bc1ed294a4e76a38a58948df60
nrcool <nomanalam7214@gmail.com>
2021-11-10 12:04:22 +0100

    add emailjs services

--- React-live-Coding/React-E-Commerce/src/components/Contact.js
+++ React-live-Coding/React-E-Commerce/src/components/Contact.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React,{useRef, useState} from "react";
+import EmailJS from "emailjs-com"
 
 export default function Contact() {
-    return (
-        <div>
-            <h1>Contact Us</h1>
-            <form >
-                <label htmlFor="name" > Name: </label>
-                <input type="text" name="name" id="name" /><br />
+    const formRef= useRef()
 
-                <label>Email:   <input type="email" name="email" /> </label>
-                <br />
+    const [status,setStatus] = useState(false)
 
-                <label>Message: </label><br />
-                <textarea name="message" cols="30" rows="10"></textarea><br />
-                <input type="submit" value="send email"/>
-                
-                
 
-            </form>
-        </div>
-    )
+    const sendEmail=(e)=>{
+
+    
+        e.preventDefault()
+
+         EmailJS.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, formRef.current, process.env.REACT_APP_USER_ID)
+        .then((result)=>{
+            if(result.text==="OK"){
+                setStatus(true)
+            }
+        })
+        .catch(err=>console.log(err))
+    }
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      {status ? <h1>Succesfully sent! Thank you for your email </h1> : <form ref={formRef} onSubmit={sendEmail}>
+        <label htmlFor="name"> Name: </label>
+        <input type="text" name="user_name" id="name" />
+        <br />
+
+        <label>
+          Email: <input type="email" name="user_email" />{" "}
+        </label>
+        <br />
+
+        <label>Message: </label>
+        <br />
+        <textarea name="message" cols="30" rows="10"></textarea>
+        <br />
+        <button>send email</button>
+      </form> }
+     
+    </div>
+  );
 }
