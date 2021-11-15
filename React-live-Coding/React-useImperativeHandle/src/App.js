8943a256996e62f2232dd17487eeda147cd7c521
nrcool <nomanalam7214@gmail.com>
2021-11-15 10:40:58 +0100

    useRef , onChange event and get form data using ref

---
+++ React-live-Coding/React-useImperativeHandle/src/App.js
@@ -0,0 +1,50 @@
+import React, { useRef,useState } from "react";
+
+function App() {
+  const formRef = useRef();
+  const [user, setUser] = useState(null);
+  const [skills, setSkills] = useState([]);
+
+  const getFormData = (e) => {
+    e.preventDefault();
+    console.log(formRef.current.username.value);
+    console.log(formRef.current.email.value);
+    console.log(formRef.current.password.value);
+    let mySkills = [];
+    if (formRef.current.html.checked) {
+      console.log("user added this html skill");
+      mySkills.push(formRef.current.html.value);
+    }
+    if (formRef.current.react.checked) {
+      console.log("user added this react skill");
+      mySkills.push(formRef.current.react.value);
+    }
+
+    setUser({
+      username: formRef.current.username.value,
+      email: formRef.current.email.value,
+      password: formRef.current.password.value,
+      skills: mySkills,
+    });
+  };
+
+  console.log(user);
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <form ref={formRef} onSubmit={getFormData}>
+        <input type="text" name="username" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="password" name="password" />
+        <br />
+        <input type="checkbox" name="html" value="html" /> html <br />
+        <input type="checkbox" name="react" value="react" /> React <br />
+        <button>submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
