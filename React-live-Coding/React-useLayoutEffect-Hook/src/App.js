86ef3c8d984d88a351a90e5444f71b470bb414f9
nrcool <nomanalam7214@gmail.com>
2021-11-16 11:30:42 +0100

    loading state

--- React-live-Coding/React-useLayoutEffect-Hook/src/App.js
+++ React-live-Coding/React-useLayoutEffect-Hook/src/App.js
@@ -1,23 +1,36 @@
 
-import React,{ useLayoutEffect,useRef} from 'react';
+import React,{ useEffect, useState} from 'react';
 
 function App() {
 
-const header= useRef()
+const [users,setUsers]=useState([])
+const [loading,setLoading]=useState(true)
 
-
-  useLayoutEffect(()=>{
-      header.current.style.backgroundColor="blue"
-      header.current.style.color="white"
-      header.current.style.position="relative"
-      header.current.style.top="300px"
+  useEffect(()=>{
+   
+    fetch("https://reqres.in/api/users?delay=3")
+    .then(res=>res.json())
+    .then(result=> {
+      setUsers(result.data)
+      setLoading(false)
+    })
 
   },[])
 
 
   return (
     <div className="App">
-      <h1 ref={header}>My React App</h1>
+      <h1>My React App</h1>
+      <div>
+        {loading ? <h2>Loading ..........</h2> : users.map(user=>{
+          return(
+            <div>
+              <img src={user.avatar} alt="" width="100" />
+              <h2>{user.first_name}</h2>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
