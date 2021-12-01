23a69747eeed7e20e1fe0867240cc74024a3a734
nrcool <nomanalam7214@gmail.com>
2021-12-01 09:46:29 +0100

    cli command

---
+++ NODEJS-Living.Coding/NodeJS-CLI-Command/index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+//shebang
+
+console.log("hello world")
+
+
+
+//1. add shebang inside your file on top
+//2. add bin property inside package.json
+//3 . sudo npm link
+
+
+//sudo npm unlink => delete that CLI from your system
