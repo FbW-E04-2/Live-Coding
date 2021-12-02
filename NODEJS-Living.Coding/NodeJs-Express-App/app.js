9de287d279945351e58d1bcad95521ce6ac629e0
nrcool <nomanalam7214@gmail.com>
2021-12-02 12:08:10 +0100

    express basic app

---
+++ NODEJS-Living.Coding/NodeJs-Express-App/app.js
@@ -0,0 +1,39 @@
+const express= require("express")
+
+//create express server
+const app = express()
+
+app.use(express.static(__dirname+"/static"))
+
+const PORT = process.env.PORT || 4000;
+
+//routes /endpoints /
+app.get("/",(req,res)=>{
+  /*   res.write()
+    res.end() */
+    res.sendFile(__dirname+"/index.html")
+})
+
+//routes /endpoints /about
+app.get("/about",(req,res)=>{
+    res.sendFile("./about.html",{root:__dirname})
+})
+
+//routes /endpoints /profile
+app.get("/profile",(req,res)=>{
+    res.sendFile(__dirname+"/profile.html")
+})
+
+//routes /endpoints /users
+app.get("/users",(req,res)=>{
+
+    const data= {name:"naqvi",age:32}
+    /* res.send(data) */
+    res.json(data)
+
+   /*  res.sendFile(__dirname+"/data.json") */
+})
+
+
+
+app.listen(PORT,()=>console.log("server is running on port : ",PORT))
