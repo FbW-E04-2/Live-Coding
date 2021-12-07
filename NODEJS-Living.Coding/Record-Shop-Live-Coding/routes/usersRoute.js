232f37d809ad065dd48d9fb6e3fc56ee718b8d7c
nrcool <nomanalam7214@gmail.com>
2021-12-07 12:11:15 +0100

    storing and reading data from file using fs

--- NODEJS-Living.Coding/Record-Shop-Live-Coding/routes/usersRoute.js
+++ NODEJS-Living.Coding/Record-Shop-Live-Coding/routes/usersRoute.js
@@ -1,30 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const path=require("path")
+const fs = require("fs")
 
+const data = fs.readFileSync(path.resolve(__dirname, "../models/db.json"),"utf-8")
 
-let users = [ 
-    {
-        "id": 7,
-        "email": "michael.lawson@reqres.in",
-        "first_name": "Michael",
-        "last_name": "Lawson",
-        "avatar": "https://reqres.in/img/faces/7-image.jpg"
-        },
-        {
-        "id": 8,
-        "email": "lindsay.ferguson@reqres.in",
-        "first_name": "Lindsay",
-        "last_name": "Ferguson",
-        "avatar": "https://reqres.in/img/faces/8-image.jpg"
-        },
-        {
-        "id": 9,
-        "email": "tobias.funke@reqres.in",
-        "first_name": "Tobias",
-        "last_name": "Funke",
-        "avatar": "https://reqres.in/img/faces/9-image.jpg"
-        }
-]
+console.log(JSON.parse(data))
+const users = JSON.parse(data).users;
 
 // CRUD operation
 //Create
@@ -43,6 +25,10 @@ router.get("/",(req,res)=>{
 router.post("/",(req,res)=>{
     console.log(req.body)
     users.push(req.body)
+    fs.writeFileSync(
+        path.resolve(__dirname, "../models/db.json"), 
+        JSON.stringify({users},null, 2)
+        )
     res.send("new user is created" )
 
 })
