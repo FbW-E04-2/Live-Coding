db24c43b4d159594ef089379333598b712967b41
nrcool <nomanalam7214@gmail.com>
2022-01-31 12:04:24 +0100

    cache data received from mongodb

--- NODEJS-Living.Coding/NodeJs-Redis-Cache/app.js
+++ NODEJS-Living.Coding/NodeJs-Redis-Cache/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 const axios = require("axios");
+const mongoose = require("mongoose")
 const { createClient } = require("redis");
 
+mongoose.connect("mongodb://127.0.0.1:27017/new-record-shop",()=>{
+    console.log("connected to database");
+})
 const client = createClient();
 
 client.on("error", (err) => {
@@ -12,15 +16,19 @@ client.on("error", (err) => {
 
 client.connect();
 
-app.get("/users", async (req, res) => {
+app.get("/records", async (req, res) => {
     //check users in cache(Redis)
-  const existingUsers = await client.get("users");
-    if(existingUsers){
-        res.send({users:existingUsers})
+  const existingRecords = await client.get("records");
+    if(existingRecords){
+        res.send({records:JSON.parse(existingRecords)})
     }else{
-        const response= await axios.get("https://jsonplaceholder.typicode.com/users")
-        await client.set("users",JSON.stringify(response.data))
-        res.send({users:response.data})
+        /* const response= await axios.get("https://jsonplaceholder.typicode.com/users")
+        await client.set("users",JSON.stringify(response.data)) */
+
+       let records = await mongoose.connection.useDb("new-record-shop").collection("records").find().toArray()
+       await client.set("records",JSON.stringify(records))
+
+        res.send({records:records})
     }
   
 });
